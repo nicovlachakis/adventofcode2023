bb152f0123737c74f1135b1b0b3bd08eff11958e
Nico Vlachakis <nico.vlachakis@nvl-it-solutions.be>
2023-12-05 14:59:27 +0100

    Final solution

--- day1/get_all_digits.py
+++ day1/get_all_digits.py
@@ -1,48 +1,25 @@
-import pdb
 import re
 
-def convert_to_digit(digit_text):    
-    conversion_table = {}
-    conversion_table["one"] = 1
-    conversion_table["two"] = 2
-    conversion_table["three"] = 3
-    conversion_table["four"] = 4
-    conversion_table["five"] = 5
-    conversion_table["six"] = 6
-    conversion_table["seven"] = 7
-    conversion_table["eight"] = 8
-    conversion_table["nine"] = 9
+conversion_table = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e",
+}
 
-    conversion_table["1"] = 1
-    conversion_table["2"] = 2
-    conversion_table["3"] = 3
-    conversion_table["4"] = 4
-    conversion_table["5"] = 5
-    conversion_table["6"] = 6
-    conversion_table["7"] = 7
-    conversion_table["8"] = 8
-    conversion_table["9"] = 9
+def replace_words(word):
+    for k, v in conversion_table.items():
+        replaced_word = word.replace(k, v)
+    return replaced_word
 
-    return conversion_table.get(digit_text, None)
+def calibration(text):
+    return sum(int(l[0] + l[-1]) for l in re.sub(r"[A-z]", "", text).split("\n"))
 
-total_sum = 0
-
-with open("data.txt", "r") as my_file:
-    data = my_file.readlines()
-
-    for tmp_line in data:
-        line = tmp_line.strip("\n")
-        digits = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line)
-        
-        if digits:                        
-            first_digit = convert_to_digit(digits[0])
-            last_digit = convert_to_digit(digits[-1]) 
-
-            if first_digit is not None and last_digit is not None:                       
-                concat_digits = int(f"{first_digit}{last_digit}")
-                total_sum += concat_digits
-            
-        
-        
-pdb.set_trace()
-
+data = open("data.txt").read()
+print(calibration(data))
+print(calibration(replace_words(data)))
